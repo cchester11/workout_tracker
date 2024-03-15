6ad9a6c7210877b674522a73daa9058258e5b1f1
Charles <cchester1@angelo.edu>
2024-03-14 20:18:04 -0500

    local postgres server running | migrations executed

--- workout_tracker/workout_tracker/settings.py
+++ workout_tracker/workout_tracker/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'workout_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user',
+        'USER': 'user',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
