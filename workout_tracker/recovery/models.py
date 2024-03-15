05facb1dd6512fdf2e93aef2ff06c9410b6f080f
Charles <cchester1@angelo.edu>
2024-03-14 21:45:15 -0500

    recovery tables written | recovery app installed in settings.py | migrations applied

--- workout_tracker/recovery/models.py
+++ workout_tracker/recovery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.models import Workout
+
 
 # Create your models here.
+class Recover(models.Model):
+      workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+      DURATION_CHOICES = [
+            ("15", "15 minutes"),
+            ("30", "30 minutes"),
+            ("45", "45 minutes"),
+            ("60", "1 hour"),
+            # Add more options as needed
+      ]
+      duration = models.CharField(max_length=2, choices=DURATION_CHOICES)
