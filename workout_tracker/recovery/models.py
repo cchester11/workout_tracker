614566dd97712533cf5a89bc4f206e8020702121
Charles <cchester1@angelo.edu>
2024-03-16 12:36:33 -0500

    added sauna or ice bath option to recovery table

--- workout_tracker/recovery/models.py
+++ workout_tracker/recovery/models.py
@@ -13,3 +13,9 @@ class Recover(models.Model):
             # Add more options as needed
       ]
       duration = models.CharField(max_length=2, choices=DURATION_CHOICES)
+      TYPE_CHOICES = [
+            ("none", "None"),
+            ("sauna", "Sauna"),
+            ("ice_bath", "Ice Bath")
+      ]
+      type = models.CharField(max_length = 10, choices=TYPE_CHOICES, default = "none")
