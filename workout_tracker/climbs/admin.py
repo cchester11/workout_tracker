9e78ff18df706d1960b82aa646d4d28b454a92b8
Charles <cchester1@angelo.edu>
2024-03-16 12:14:42 -0500

    all models registered to admin

--- workout_tracker/climbs/admin.py
+++ workout_tracker/climbs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Climbing, KilterBoarding, FreeClimb
+
+admin.site.register(Climbing)
+admin.site.register(KilterBoarding)
+admin.site.register(FreeClimb)
